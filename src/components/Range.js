516c8928de075ceedadf303e8279ade9b3190bbf
Toni <toni.am.martinez@gmail.com>
2021-06-18 23:27:28 +0200

    [dev] remove labels and update values of inputs

--- src/components/Range.js
+++ src/components/Range.js
@@ -4,10 +4,10 @@ class Range extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            // currentMin: 10,
             currentMin: props.min,
-            // currentMax: 80
-            currentMax: props.max
+            tempCurrentMin: props.min,
+            currentMax: props.max,
+            tempCurrentMax: props.max
         }
     }
 
@@ -18,71 +18,77 @@ class Range extends React.Component {
 
     calculateWidthPercent() {
         const diff = this.props.max - this.props.min
-        return parseFloat((100 * (this.state.currentMax - this.state.currentMin) / diff).toFixed(2))
+        const currentDiffMinOne = (this.state.currentMax - this.state.currentMin) > 1 ? this.state.currentMax - this.state.currentMin : 1
+        return parseFloat((100 * (currentDiffMinOne) / diff).toFixed(2))
     }
 
     handleSetMin(value) {
-        if (isFinite(value)) {
-            if (value < this.props.min) value = this.props.min
-            if (value > this.state.currentMax) value = this.state.currentMax
-            this.setState({showInputCurrentMin: false, currentMin: value})
+        if (value !== '' && isFinite(value)) {
+            if (parseFloat(value) < parseFloat(this.props.min)) value = this.props.min
+            if (parseFloat(value) > parseFloat(this.state.currentMax)) value = this.state.currentMax
+            this.setState({showInputCurrentMin: false, currentMin: value, tempCurrentMin: value})
         }
         else
-            this.setState({showInputCurrentMin: false})
+            this.setState({tempCurrentMin: this.state.currentMin})
 
     }
 
     handleSetMax(value) {
-        if (isFinite(value)) {
-            if (value > this.props.max) value = this.props.max
-            if (value < this.state.currentMin) value = this.state.currentMin
-            this.setState({showInputCurrentMax: false, currentMax: value})
+        if (value !== '' && isFinite(value)) {
+            if (parseFloat(value) > parseFloat(this.props.max)) value = this.props.max
+            if (parseFloat(value) < parseFloat(this.state.currentMin)) value = this.state.currentMin
+            this.setState({showInputCurrentMax: false, currentMax: value, tempCurrentMax: value})
         }
         else
-            this.setState({showInputCurrentMax: false})
+            this.setState({tempCurrentMax: this.state.currentMax})
     }
 
 
     render() {
         return (
-            <div className="input-range" data-cy="input-range__wrapper">
-                <div className="input-range__bar-background" data-cy="input-range__bar-background">
-                    <div style={{left: this.calculateLeftPercent() + '%', width: this.calculateWidthPercent() + '%'}} className="input-range__bar" data-cy="input-range__bar">
-                        {this.state.showInputCurrentMin ?
-                            <div className="input-range__input input-range__input--min" data-cy="input-range__input-min">
-                                <input onKeyDown={(e) => {
-                                    if (e.key === 'Enter' || e.code === '13' || e.keyCode === 13) this.handleSetMin(e.currentTarget.value)
-                                }}
-                                       onBlur={(e) => this.handleSetMin(e.target.value)}
-                                       defaultValue={this.state.currentMin}
-                                       autoFocus={true}
-                                />
-                            </div>
-                            :
-                            <div className="input-range__label input-range__label--min" data-cy="input-range__label-min">
-                                <span onClick={() => this.setState({showInputCurrentMin: true})}>{this.state.currentMin}</span>
-                            </div>
-
-                        }
-                        {this.state.showInputCurrentMax ?
-                            <div className="input-range__input input-range__input--max" data-cy="input-range__input-max">
-                                <input onKeyDown={(e) => {
-                                    if (e.key === 'Enter' || e.code === '13' || e.keyCode === 13) this.handleSetMax(e.currentTarget.value)
-                                }}
-                                       onBlur={(e) => this.handleSetMax(e.target.value)}
-                                       defaultValue={this.state.currentMax}
-                                       autoFocus={true}
-                                />
-                            </div>
-                            :
-                            <div className="input-range__label input-range__label--max" data-cy="input-range__label-max">
-                                <span onClick={() => this.setState({showInputCurrentMax: true})}>{this.state.currentMax}</span>
-                            </div>
-                        }
+            <div>
+                <div className="input-range" data-cy="input-range__wrapper">
+                    <div className="input-range__input input-range__input--min" data-cy="input-range__input-min">
+                        <input onKeyDown={(e) => {
+                            if (e.key === 'Enter' || e.code === '13' || e.keyCode === 13) this.handleSetMin(e.currentTarget.value)
+                        }}
+                               onBlur={(e) => this.handleSetMin(e.target.value)}
+                               onChange={(e) => this.setState({tempCurrentMin: e.target.value})}
+                               size={this.props.max ? this.props.max.toString().length : 1}
+                               value={this.state.tempCurrentMin}
+                               autoFocus={true}
+                        />
                     </div>
+
+
+                    <div className="input-range__bar-background" data-cy="input-range__bar-background">
+                        <div style={{left: this.calculateLeftPercent() + '%', width: this.calculateWidthPercent() + '%', minWidth: '30px'}} className="input-range__bar"
+                             data-cy="input-range__bar">
+                            <div className="wrapper_bullet">
+                                <div className="input-range__bullet input-range__bullet--min" data-cy="input-range__bullet-min"/>
+                            </div>
+                            <div className="wrapper_bullet">
+                                <div className="input-range__bullet input-range__bullet--max" data-cy="input-range__bullet-max" style={{backgroundColor: 'red'}}/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="input-range__input input-range__input--max" data-cy="input-range__input-max">
+                        <input onKeyDown={(e) => {
+                            if (e.key === 'Enter' || e.code === '13' || e.keyCode === 13) this.handleSetMax(e.currentTarget.value)
+                        }}
+                               onBlur={(e) => this.handleSetMax(e.target.value)}
+                               onChange={(e) => this.setState({tempCurrentMax: e.target.value})}
+                               size={this.props.max ? this.props.max.toString().length : 1}
+                               value={this.state.tempCurrentMax}
+                               autoFocus={true}
+                        />
+                    </div>
+
                 </div>
-                {/*//TODO: quitar el botón de pruebas*/}
-                <div style={{marginTop: '50px'}} onClick={() => this.setState({currentMin: 20, currentMax: 80})}>change state</div>
+                {/*//TODO: quitar el botón de pruebas */}
+                <div style={{marginTop: '50px'}} onClick={() => this.setState({currentMin: 20, tempCurrentMin: 20, currentMax: 80, tempCurrentMax: 80})}>change state</div>
+                {/*//TODO: quitar el botón de pruebas */}
             </div>
         )
     }
--- src/components/Range.test.js
+++ src/components/Range.test.js
@@ -11,6 +11,9 @@ configure({adapter: new Adapter()})
 describe('Range suit', () => {
     afterEach(cleanup)
 
+    const selectorInputMin = '[data-cy=input-range__input-min] input'
+    const selectorInputMax = '[data-cy=input-range__input-max] input'
+
     test('Render ok', () => {
         const {container} = render(<Range/>)
         expect(container.childElementCount).toBeGreaterThan(0)
@@ -22,102 +25,54 @@ describe('Range suit', () => {
         const wrapper = container.querySelectorAll('[data-cy=input-range__wrapper]')
         const bar = container.querySelectorAll('[data-cy=input-range__bar]')
         const barBackground = container.querySelectorAll('[data-cy=input-range__bar-background]')
-        const labelMin = container.querySelectorAll('[data-cy=input-range__label-min]')
-        const labelMax = container.querySelectorAll('[data-cy=input-range__label-max]')
+        const inputMin = container.querySelectorAll('[data-cy=input-range__input-min]')
+        const inputMax = container.querySelectorAll('[data-cy=input-range__input-max]')
+        const bulletMin = container.querySelectorAll('[data-cy=input-range__bullet-min]')
+        const bulletMax = container.querySelectorAll('[data-cy=input-range__bullet-max]')
 
         expect(wrapper.length).toEqual(1)
         expect(bar.length).toEqual(1)
         expect(barBackground.length).toEqual(1)
-        expect(labelMin.length).toEqual(1)
-        expect(labelMax.length).toEqual(1)
-    })
-
-    test('show min and max labels', () => {
-
-        const {container} = render(<Range min={1} max={10}/>)
-
-        const labelMin = container.querySelector('[data-cy=input-range__label-min]')
-        const labelMax = container.querySelector('[data-cy=input-range__label-max]')
-
-        expect(labelMin).toHaveTextContent('1')
-        expect(labelMax).toHaveTextContent('10')
-    })
-
-    test('show input on click labels and hidden when lost focus or push Enter', () => {
-        const {container} = render(<Range min={1} max={10}/>)
-
-        fireEvent.click(container.querySelector('[data-cy=input-range__label-min] span'))
-        expect(container.querySelector('[data-cy=input-range__input-min]')).not.toBeNull()
-        fireEvent.blur(container.querySelector('[data-cy=input-range__input-min] input'))
-        expect(container.querySelector('[data-cy=input-range__input-min]')).toBeNull()
-
-        fireEvent.click(container.querySelector('[data-cy=input-range__label-min] span'))
-        expect(container.querySelector('[data-cy=input-range__input-min]')).not.toBeNull()
-        fireEvent.keyDown(container.querySelector('[data-cy=input-range__input-min] input'), {key: "Enter", code: 13})
-
-        expect(container.querySelector('[data-cy=input-range__input-min]')).toBeNull()
-
-
-        fireEvent.click(container.querySelector('[data-cy=input-range__label-max] span'))
-        expect(container.querySelector('[data-cy=input-range__input-max]')).not.toBeNull()
-        fireEvent.blur(container.querySelector('[data-cy=input-range__input-max] input'))
-        expect(container.querySelector('[data-cy=input-range__input-max]')).toBeNull()
-
-        fireEvent.click(container.querySelector('[data-cy=input-range__label-max] span'))
-        expect(container.querySelector('[data-cy=input-range__input-max]')).not.toBeNull()
-        fireEvent.keyDown(container.querySelector('[data-cy=input-range__input-max] input'), {key: "Enter", code: 13})
-        expect(container.querySelector('[data-cy=input-range__input-max]')).toBeNull()
+        expect(inputMin.length).toEqual(1)
+        expect(inputMax.length).toEqual(1)
+        expect(bulletMin.length).toEqual(1)
+        expect(bulletMax.length).toEqual(1)
     })
 
     const probesToSetInput = [
-        {min: 1, changeMin: undefined, changeMax: undefined, max: 100, expectedMin: 1, expectedMax: 100},
-        {min: 1, changeMin: 25, changeMax: 50, max: 100, expectedMin: 25, expectedMax: 50},
-        {min: 1, changeMin: 'string', changeMax: 'string', max: 100, expectedMin: 1, expectedMax: 100},
-        {min: 1, changeMin: -20, changeMax: 200, max: 100, expectedMin: 1, expectedMax: 100},
-        {min: 1, changeMin: 150, changeMax: 50, max: 100, expectedMin: 100, expectedMax: 100},
+        {min: '1', changeMin: '', changeMax: '', max: '100', expectedMin: '1', expectedMax: '100'},
+        {min: '1', changeMin: '25', changeMax: '50', max: '100', expectedMin: '25', expectedMax: '50'},
+        {min: '1', changeMin: 'string', changeMax: 'string', max: '100', expectedMin: '1', expectedMax: '100'},
+        {min: '1', changeMin: '-20', changeMax: '200', max: '100', expectedMin: '1', expectedMax: '100'},
+        {min: '1', changeMin: '150', changeMax: '50', max: '100', expectedMin: '100', expectedMax: '100'},
     ]
     probesToSetInput.forEach(({min, changeMin, changeMax, max, expectedMin, expectedMax}) => {
         test(`Change current values with inputs ${min + ', ' + changeMin + ', ' + changeMax + ', ' + max + ', ' + expectedMin + ', ' + expectedMax}`, () => {
             const {container} = render(<Range min={1} max={100}/>)
 
-            const selectorLabelMin = '[data-cy=input-range__label-min] span'
-            const selectorInputMin = '[data-cy=input-range__input-min] input'
-            const selectorLabelMax = '[data-cy=input-range__label-max] span'
-            const selectorInputMax = '[data-cy=input-range__input-max] input'
 
-            fireEvent.click(container.querySelector(selectorLabelMin))
             fireEvent.change(container.querySelector(selectorInputMin), {target: {value: changeMin}})
             fireEvent.blur(container.querySelector(selectorInputMin))
-            expect(container.querySelector(selectorLabelMin)).toHaveTextContent(expectedMin)
+            expect(container.querySelector(selectorInputMin)).toHaveValue(expectedMin)
 
-
-            fireEvent.click(container.querySelector(selectorLabelMax))
             fireEvent.change(container.querySelector(selectorInputMax), {target: {value: changeMax}})
             fireEvent.blur(container.querySelector(selectorInputMax))
-            expect(container.querySelector(selectorLabelMax)).toHaveTextContent(expectedMax)
+            expect(container.querySelector(selectorInputMax)).toHaveValue(expectedMax)
         })
     })
 
     test(`Change value when are equals`, () => {
         const {container} = render(<Range min={1} max={100}/>)
 
-        const selectorLabelMin = '[data-cy=input-range__label-min] span'
-        const selectorInputMin = '[data-cy=input-range__input-min] input'
-        const selectorLabelMax = '[data-cy=input-range__label-max] span'
-        const selectorInputMax = '[data-cy=input-range__input-max] input'
 
         function changeMin(value) {
-            fireEvent.click(container.querySelector(selectorLabelMin))
             fireEvent.change(container.querySelector(selectorInputMin), {target: {value: value}})
             fireEvent.blur(container.querySelector(selectorInputMin))
-            expect(container.querySelector(selectorLabelMin)).toHaveTextContent(value)
         }
 
         function changeMax(value) {
-            fireEvent.click(container.querySelector(selectorLabelMax))
             fireEvent.change(container.querySelector(selectorInputMax), {target: {value: value}})
             fireEvent.blur(container.querySelector(selectorInputMax))
-            expect(container.querySelector(selectorLabelMax)).toHaveTextContent(value)
         }
 
         changeMin(50)
@@ -125,6 +80,20 @@ describe('Range suit', () => {
         changeMax(80)
         changeMin(80)
         changeMin(20)
+    })
+    const probesSize = [
+        {max: 5, expected: '1'},
+        {max: 10, expected: '2'},
+        {max: 100, expected: '3'},
+    ]
+    probesSize.forEach(({max, expected}) => {
+        test(`Check sizes input ${max + ', ' + expected}`, () => {
+            const {container} = render(<Range min={1} max={max}/>)
+
+            expect(container.querySelector(selectorInputMin).getAttribute('size')).toBe(expected)
+            expect(container.querySelector(selectorInputMax).getAttribute('size')).toBe(expected)
+
+        })
     })
 
 
@@ -145,7 +114,8 @@ describe('Range suit', () => {
     const probesMaxPercent = [
         {min: 0, currentMin: 0, currentMax: 100, max: 100, expected: 100},
         {min: 0, currentMin: 20, currentMax: 100, max: 100, expected: 80},
-        {min: 0, currentMin: 25, currentMax: 75, max: 100, expected: 50}
+        {min: 0, currentMin: 25, currentMax: 75, max: 100, expected: 50},
+        {min: 0, currentMin: 25, currentMax: 25, max: 100, expected: 1},
     ]
     probesMaxPercent.forEach((probe) => {
         test(`calculateMaxPercent ${probe.min} ${probe.currentMin} ${probe.currentMax} ${probe.max}`, () => {
